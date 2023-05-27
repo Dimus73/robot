ea457f3122deda100da8a17ca73dc8618440c5b5
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-27 08:32:21 +0300

    0001

--- script01.js
+++ script01.js
@@ -26,6 +26,7 @@ for (let robot of robots){
   cards.appendChild(card);    
 }
 
+//Screen width control function. Change the indent of the block of cards from the top of the screen.
 window.addEventListener('resize', function(){
   if (window.innerWidth < 1030){
     cards.style.marginTop = '150px';
@@ -75,7 +76,12 @@ function createCardsConteyner(){
   cards.style.justifyContent = 'space-around'
   cards.style.flexWrap = 'wrap';
   cards.style.margin ='0px auto';
-  cards.style.marginTop = '100px';
+  // cards.style.marginTop = '100px';
+  if (window.innerWidth < 1030){
+    cards.style.marginTop = '180px';
+  } else {
+    cards.style.marginTop = '100px';
+  }
 
   return cards;
 }
